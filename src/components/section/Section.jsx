d2324964a6b901c17fc6bb924ebe3383b50047b1
ldulivo <ldulivo@gmail.com>
2022-03-23 11:01:43 -0300

    test collage

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 
 export default function Section({ backGround = 'primary', children }) {
   return (
-    <>
-      <div className="section--header"></div>
-      <section className={backGround}>
-        {
-          ( backGround === 'primary')
-            ? children
-            : <div className="section--container">{ children }</div>
-        }
-      </section>
-    </>
+    <section className={backGround}>
+      {
+        ( backGround === 'primary')
+          ? children
+          : <div className="section--container">{ children }</div>
+      }
+    </section>
   )
 }
