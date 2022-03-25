03156eafcfb294ccd8292c1249ee84b65cab4d89
ldulivo <ldulivo@gmail.com>
2022-03-25 20:03:40 -0300

    Change number Whatsapp and msg

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import Whatsapp from '../../svg/Whatsapp'
+//href="https://wa.me/5491139102975?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio" 
 
 export default function Button({ className, children }) {
+  const msg = 'Hola Pablo! Estoy interesado en tu curso';
+  const number = '5491139102975'
   return (
     <a 
-      href="https://wa.me/54110000000?text=Hola!%20Estoy%20interesado%20en%20tu%20servicio" 
+      href={`https://wa.me/${number}?text=${msg}`} 
       target="_blank" 
       rel="noopener noreferrer"
     >
