07139ca4de3cf779c68df629a3c602eecbe8a257
ldulivo <ldulivo@gmail.com>
2022-03-26 02:08:23 -0300

    window.scrollY added > 300 in float button

--- src/components/floatButton/FloatButton.jsx
+++ src/components/floatButton/FloatButton.jsx
@@ -1,19 +1,37 @@
+import { useState } from 'react'
 import Whatsapp from '../../svg/Whatsapp'
 
 export default function FloatButton() {
     const msg = '¡Hola Pablo! Estoy interesado en la Formación Transgeneracional';
     const number = '5491139102975';
 
+    const [scrollY, setScrollY] = useState(window.scrollY);
+
+    window.onscroll = () => {
+      setScrollY( window.scrollY )
+    }
+
+
   return (
-      <a 
-        href={`https://wa.me/${number}?text=${msg}`} 
-        target="_blank" 
-        rel="noopener noreferrer"
-        className='FloatButton'
-      >
-        <button className='FloatButton--btn'>
-            <Whatsapp />
-        </button>
-      </a>
+
+    <>
+      {
+        ( scrollY > 300 )
+        ? 
+          <a 
+          href={`https://wa.me/${number}?text=${msg}`} 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className='FloatButton'
+        >
+          <button className='FloatButton--btn'>
+              <Whatsapp />
+          </button>
+        </a>
+        : null
+      }
+      
+    </>
+      
   )
 }
